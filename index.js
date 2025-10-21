676ed8eef4ffd2902e3539a6b4653d6652a55899
MuhammadShidqulIman <muhammadshidqul3@gmail.com>
2025-10-21 10:24:58 +0700

    menambahkan endpoint POST /api/mahasiswa untuk input data mahasiswa ke database

--- index.js
+++ index.js
@@ -26,16 +26,36 @@ db.connect((err) => {
     console.error('error connecting to MySQL: ' + err.stack);
     return;
   }
-  console.log('connection Succuesfully!');
+  console.log('connection Succuessfully!');
 })
 
 app.get('/api/mahasiswa', (req, res) => {
   db.query('SELECT * FROM mahasiswa', (err, results) => {
     if (err) {
-      console.error('Error executing query: ' + err.satck);
+      console.error('Error executing query: ' + err.stack);
       res.status(500).send('error fetching users');
       return;
     }
     res.json(results);
   })
 })
+
+app.post('/api/mahasiswa', (req, res) => {
+  const { nama, nim, kelas, prodi } = req.body;
+
+  if (!nama || !nim || !kelas ||!prodi) {
+    return res.status(400).json({message: 'nama, nim, kelas, prodi wajib diisi' });
+  }
+
+  db.query(
+    'INSERT INTO mahasiswa (nama, nim, kelas, prodi) VALUES (?, ?, ?, ?)',
+    [nama, nim, kelas, prodi],
+    (err, result) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ message: 'Database error' });
+      }
+      res.status(201).json({ message: 'User created successfully' });
+    }
+  );
+});
